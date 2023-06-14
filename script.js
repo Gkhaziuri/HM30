a971e8672ec652d673eba1ec68aff956c4665398
Giorgi Khaziuri <gxaziuri7@gmail.com>
2023-06-14 17:52:12 +0400

    added massive minipulations

---
+++ script.js
@@ -0,0 +1,56 @@
+// დავალება N2
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // let size = sports.length;
+        // console.log(size);
+
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // document.getElementById("demo").innerHTML = sports.toString();
+        // console.log(sports)                        ?    
+
+
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // sports.pop();
+        // console.log(sports);
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // sports.push();
+        // console.log(sports);
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // sports.shift();
+        // console.log(sports);
+
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // sports.unshift("Basketball");
+        // console.log(sports);
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // sports[2] = "Soccer";
+        // console.log(sports);
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // delete sports[1];
+        // console.log(sports);
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // const fruits = ["Banana", "Apple", "orange", "Strawberry"];
+
+        // const test = sports.concat(fruits)
+        // console.log(test);
+
+        //    const numbers = [[2,4], [6,12], [8,16]];
+        //    const newNumbers = numbers.flat();
+        //    console.log(newNumbers);
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // sports.splice(2,0, "Soccer", "Golf" )
+        // console.log(sports);
+
+        // const sports = ["Football", "Basketball", "Tennis", "Rugby"];
+        // const differentBall = sports.slice(3);
+        // console.log(differentBall);
+
